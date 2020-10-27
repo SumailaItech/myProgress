f09ee08fbec818c00de15d4461bc3f7df0ce8369
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 19:15:38 +0200

    Learnt about the rest parameter and how to to used it in a function

--- moreOnFunctions/app.js
+++ moreOnFunctions/app.js
@@ -93,3 +93,23 @@ const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
      gameIsRunning = false;
  });
 
+// related to function
+
+const sumUp =(...numbers) =>{
+    let sum =0;
+    for(const num of numbers){
+        sum +=num;
+    }
+    return sum;
+}
+
+const subtractUp = function(...numbers){
+    let sum =0;
+    for(const num of numbers){
+        sum -=num;
+    }
+    return sum;
+}
+console.log(sumUp(1,5,10,-3,10));
+console.log(sumUp(1,5,10,-3,10,25,88));
+console.log(subtractUp(1,10,15,20));
