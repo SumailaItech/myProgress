4aa915f1f07ab5ff11552ed442e10eedf3429d6b
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 15:37:29 +0200

    Added User choice to the Rock, paper Scissors game

--- moreOnFunctions/app.js
+++ moreOnFunctions/app.js
@@ -4,11 +4,36 @@ const startGameBtn = document.getElementById('start-game-btn');
 //     console.log('Game starting...')
 // }
 
-const start = function startGame(){
-    console.log('Game starting...')
-}
+// const start = function startGame(){
+//     console.log('Game starting...')
+// }
 // console.log(typeof startGame);
 //console.dir(startGame);
+const ROCK ='ROCK';
+const PAPER ='PAPER';
+const SCISSORS ='SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
+let gameIsRunning = false;
 
-startGameBtn.addEventListener('click',start);
+const getPlayerChoice = function(){
+    const selection = prompt(`${ROCK},${PAPER}, OR ${SCISSORS} ?`).toUpperCase();
+    if(selection !==ROCK &&
+    selection !==PAPER &&
+    selection !==SCISSORS){
+        alert(`Invalid Chioce! we choice ${DEFAULT_USER_CHOICE} for you`);
+        return DEFAULT_USER_CHOICE
+    } 
+    return selection;
+}
+
+ startGameBtn.addEventListener('click',function(){
+     if(gameIsRunning){
+         return;
+     }
+     gameIsRunning = true;
+     console.log('Game starting...')
+     const playerSelection = getPlayerChoice();
+     console.log(playerSelection);
+ });
+
