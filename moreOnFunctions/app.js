aaa06f147c8aaef34c5d4bd8aabd768fc7ab3922
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 14:10:43 +0200

    Learnt that function in javascript can be store in a variable

--- moreOnFunctions/app.js
+++ moreOnFunctions/app.js
@@ -1,11 +1,14 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame(){
+// function startGame(){
+//     console.log('Game starting...')
+// }
+
+const start = function startGame(){
     console.log('Game starting...')
 }
-
 // console.log(typeof startGame);
-console.dir(startGame);
+//console.dir(startGame);
 
 
-startGameBtn.addEventListener('click',startGame);
+startGameBtn.addEventListener('click',start);
