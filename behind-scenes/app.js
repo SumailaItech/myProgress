3507e1be98e2570ceb7e5d392ab89dde54348a82
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 07:48:10 +0200

    learnt about how javascript read and execute scripts

--- behind-scenes/app.js
+++ behind-scenes/app.js
@@ -1,16 +1,20 @@
-let name ='Sumaila';
+// let name ='Sumaila';
 
-if(name ==='Sumaila'){
-    let hobbies =['sports','basket']
-    console.log(hobbies)
-}
-function greet(){
-    // let age = 30;
-    // let name ='Manuel';
-    var age = 30;
-    var name ='Manuel';
-    console.log(name, age);
-}
+// if(name ==='Sumaila'){
+//     let hobbies =['sports','basket']
+//     console.log(hobbies)
+// }
+// function greet(){
+//     // let age = 30;
+//     // let name ='Manuel';
+//     var age = 30;
+//     var name ='Manuel';
+//     console.log(name, age);
+// }
 
-console.log(name,hobbies);
-greet();
+// console.log(name,hobbies);
+// greet();
+
+console.log(userName);
+
+var userName ='sumaila';
