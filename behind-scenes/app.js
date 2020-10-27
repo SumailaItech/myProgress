b414863a6f30718e02aa1539a8a5e0f473fa21a0
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 07:40:02 +0200

    learnt about the different between var and let keywords

---
+++ behind-scenes/app.js
@@ -0,0 +1,16 @@
+let name ='Sumaila';
+
+if(name ==='Sumaila'){
+    let hobbies =['sports','basket']
+    console.log(hobbies)
+}
+function greet(){
+    // let age = 30;
+    // let name ='Manuel';
+    var age = 30;
+    var name ='Manuel';
+    console.log(name, age);
+}
+
+console.log(name,hobbies);
+greet();
