62f5db95dfed7962674dc16651391a2eb650da1c
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 13:37:32 +0200

    Started learning the more functions module and have finished setting up the project files

---
+++ moreOnFunctions/app.js
@@ -0,0 +1,10 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame(){
+    console.log('Game starting...')
+}
+
+// console.log(typeof startGame);
+console.dir(startGame);
+
+startGameBtn.addEventListener('click',startGame);
