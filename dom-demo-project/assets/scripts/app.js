7bae997e910e9a1c8e6c37e6c9b0c730aff00410
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 06:36:07 +0200

    Added a fetch and validate user input function the application

--- dom-demo-project/assets/scripts/app.js
+++ dom-demo-project/assets/scripts/app.js
@@ -1,8 +1,42 @@
 const startAddMovieButton = document.querySelector('header button');
 const addMovieModal =document.getElementById('add-modal');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieModal = addMovieModal.querySelector('.btn--passive')
+const confirmAddMovieBtn = cancelAddMovieModal.nextElementSibling;
+const userInputs =addMovieModal.querySelectorAll('input');
+
+const toggleBackDrop = ()=>{
+    backdrop.classList.toggle('visible');
+}
 
 const toggleMovieModal = ()=>{
     addMovieModal.classList.toggle('visible');
+    toggleBackDrop();
 };
 
+const addMovieHandler =()=>{
+    const titleValue = userInputs[0].value;
+    const imageUrlValue =userInputs[1].value;
+    const ratingValue = userInputs[2].value
+
+    if(titleValue.trim ==='' || imageUrlValue.trim ==='' ||
+     ratingValue.trim ==='' || +ratingValue < 1 || +ratingValue > 5){
+      alert("Enter valid value, rating between 1 and 5");
+      return;
+    }
+}
+
+const cancelAddMovie = () =>{
+    toggleMovieModal();
+}
+
+const backdropClickHandler = ()=>{
+    toggleMovieModal();
+}
+
+
+
 startAddMovieButton.addEventListener('click',toggleMovieModal);
+backdrop.addEventListener('click',backdropClickHandler);
+cancelAddMovieModal.addEventListener('click', cancelAddMovie);
+confirmAddMovieBtn.addEventListener('click',addMovieHandler);
