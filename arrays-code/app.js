a7c1633d896b2ee32075fab9256dee93a427bb54
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 21:04:45 +0200

    Learnt about sorting and reversing of arrays using sort and reverse methods

--- arrays-code/app.js
+++ arrays-code/app.js
@@ -90,4 +90,15 @@ const taxAdjustedPrices = prices.map((price, idx, prices) => {
     const priceOb = {index:idx, taxAdj: price * (1 + tax)};
     return priceOb;
 });
-console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a ,b) =>{
+    if(a > b){
+        return 1;
+    }else if(a ===b){
+        return 0;
+    }else{
+        return -1;
+    }
+});
+//console.log(prices, taxAdjustedPrices);
+console.log(sortedPrices.reverse());
