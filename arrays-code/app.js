c54a35147bffc49099dc206594f08645ec47de71
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 18:23:08 +0200

    Learnt about the type of data that can be store in an arrays

--- arrays-code/app.js
+++ arrays-code/app.js
@@ -16,3 +16,13 @@ console.log(listItem);
 
 const arrayListitems = Array.from(listItem);
 console.log(arrayListitems);
+
+const hobbies =['cooking','sports'];
+const personalData = [30,'max',{moreData:'info'}];
+
+const analyticsData = [[1, 1.6],[2, 4],[5, 7]];
+for(const analData of analyticsData){
+    for(const data of analData){
+        console.log(data);
+    }
+}
