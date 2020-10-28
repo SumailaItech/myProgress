cd90cb0814abc3eddc359531e70d4e6130dd6b8e
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 18:01:28 +0200

    Learnt about the different ways of creating array in javascript

---
+++ arrays-code/app.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3];
+
+console.log(numbers);
+
+// const moreNumbers = new Array(5);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1 ,2);
+// console.log(yetMoreNumbers);
+
+const moreNumbers =Array.from('Hi');
+console.log(moreNumbers);
+const listItem = document.querySelectorAll('li');
+
+console.log(listItem);
+
+const arrayListitems = Array.from(listItem);
+console.log(arrayListitems);
