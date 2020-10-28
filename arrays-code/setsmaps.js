508bb87a4bb5fd45a57a3bbaa688e784a7284409
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 23:17:52 +0200

    Learnt how to work with Map  data structure in javascript

--- arrays-code/setsmaps.js
+++ arrays-code/setsmaps.js
@@ -1,9 +1,33 @@
-const ids = new Set([1, 2, 3, 4]);
-ids.add(2);
-if(ids.has(2)){
-    ids.delete(2);
+// const ids = new Set([1, 2, 3, 4]);
+// ids.add(2);
+// if(ids.has(2)){
+//     ids.delete(2);
+// }
+
+// for(const entry of ids.entries()){
+//     console.log(entry[0]);
+// }
+
+const person1 ={name:'Sumaila'};
+const person2 = {name:'Iddrisu'};
+
+const personData =new Map([[person1,[{date:'yesterday',price:10 }]]])
+
+personData.set(person2,[{date:'Two days ago',price:100}]);
+console.log(personData);
+console.log(personData.get(person1))
+
+
+for(const [key, value] of personData.entries()){
+    console.log(key, value);
 }
 
-for(const entry of ids.entries()){
-    console.log(entry[0]);
+for(const key of personData.keys()){
+    console.log(key);
 }
+
+for(const value of personData.values()){
+    console.log(value);
+}
+
+console.log(personData.size);
