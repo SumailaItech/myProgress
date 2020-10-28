49d954e446f83f450ca2754f451a3853d76145a4
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 05:53:29 +0200

    I have Selected both the modal and the button in the index using the query selectors

---
+++ dom-demo-project/assets/scripts/app.js
@@ -0,0 +1,7 @@
+const startAddMovieButton = document.querySelector('header button');
+const addMovieModal =document.getElementById('add-moal');
+
+
+// startAddMovieButton.addEventListener('click', ()=>{
+//     addMovieModal.classList.toggle('visible');
+// })
