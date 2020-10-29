de90a58d9514aed3381b45608c88fecc299d4ddc
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-29 10:44:39 +0200

    Learnt about object destructuring and how to it to shorten a javascript code

--- objects-codes/assets/scripts/objects.js
+++ objects-codes/assets/scripts/objects.js
@@ -36,13 +36,16 @@ const renderMovies =(filter='')=>{
 
     filterMovies.forEach((movie)=>{
         const movieEl =document.createElement('li');
-        let text = movie.info.title + ' - ';
-        for(const key in movie.info){
+        const{ info, ...otherProps } = movie;
+        const { title:movieTitle } = info;
+        let text = info.title + ' - ';
+      //let text = movie.info.title + ' - ';
+        for(const key in info){
             if(key !=='title'){
-                text = text + `${key}: ${movie.info[key]}`
+                text = text + `${key}: ${info[key]}`
             }
         }
-
+ console.log(otherProps);
         movieEl.textContent = text;
         movieList.appendChild(movieEl);
     });
