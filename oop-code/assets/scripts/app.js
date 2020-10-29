08144576ffd3d4166018ff9f08a82ee447b83f75
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-29 16:29:56 +0200

    I have binded the class method to this keyword to access one product in the products

--- oop-code/assets/scripts/app.js
+++ oop-code/assets/scripts/app.js
@@ -11,12 +11,43 @@ class Product{
         this.description = desc;
         this.price = price;
     }
+   
 }
 
-console.log(new Product());
+class ProductItem{
+    product;
 
-const productList ={
-    products:[
+    constructor(product){
+        this.product = product;
+    }
+
+    addToCart(){
+        console.log('adding product');
+        console.log(this.product)
+    }
+
+    render(){
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML =`
+        <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+            <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to cart</button>
+            </div>
+        </div>
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click',this.addToCart.bind(this));
+        return prodEl;
+    }
+}
+
+class ProductList{
+    products = [
         new Product(
             'A nice pillow',
             'file:///C:/Users/Huseina/Desktop/amalitech%20projects/myProgress/oop-code/assets/images/pillow.jpg',
@@ -29,6 +60,38 @@ const productList ={
             'A beautiful carpet you may like',
             89.9,
         )
+    ];
+
+    constructor(){}
+
+    render(){
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className ='product-list';
+        for(const prod of this.products){
+          const productsItem = new ProductItem(prod);
+          const prodEl =productsItem.render();
+            prodList.appendChild(prodEl);
+        }
+        renderHook.appendChild(prodList);
+    }
+
+}
+
+//const productList ={
+    // products:[
+    //     new Product(
+    //         'A nice pillow',
+    //         'file:///C:/Users/Huseina/Desktop/amalitech%20projects/myProgress/oop-code/assets/images/pillow.jpg',
+    //         'A very soft pillow for good sleeping',
+    //         19.9
+    //     ),
+    //     new Product(
+    //         'A beutiful Carpet',
+    //         'file:///C:/Users/Huseina/Desktop/amalitech%20projects/myProgress/oop-code/assets/images/carpet.jpg',
+    //         'A beautiful carpet you may like',
+    //         89.9,
+    //     )
         // {
         //     title:'A nice pillow',
         //     imageUrl:'file:///C:/Users/Huseina/Desktop/amalitech%20projects/myProgress/oop-code/assets/images/pillow.jpg',
@@ -41,30 +104,11 @@ const productList ={
         //     price:89.9,
         //     description:'A beautiful carpet you may like'
         // }
-    ],
+   // ],
 
-    render(){
-        const renderHook = document.getElementById('app');
-        const prodList = document.createElement('ul');
-        prodList.className ='product-list';
-        for(const prod of this.products){
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML =`
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                <div class="product-item__content">
-                    <h2>${prod.title}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to cart</button>
-                </div>
-            </div>
-            `
-            prodList.appendChild(prodEl);
-        }
-        renderHook.appendChild(prodList);
-    }
-}
-
+//     render(){
+       
+//     }
+// }
+const productList = new ProductList();
 productList.render();
