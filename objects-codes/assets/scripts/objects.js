2558c448ed37581615b54eaf0ec207e34334952b
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-29 07:43:38 +0200

    I have created a movie and  render the movies added on to the browser

--- objects-codes/assets/scripts/objects.js
+++ objects-codes/assets/scripts/objects.js
@@ -1,18 +1,70 @@
-const movieList = document.getElementById('movie-list');
+// const movieList = document.getElementById('movie-list');
 
-movieList.style.backgroundColor = 'red';
-movieList.style.display ='block';
+// movieList.style.backgroundColor = 'red';
+// movieList.style.display ='block';
 
-const person ={
-    'first name':'Sumaila',
-    age:30,
-    hobies:['Coding','eating','football'],
-    greet:function(){
-        alert("Hi there");
+// const person ={
+//     'first name':'Sumaila',
+//     age:30,
+//     hobies:['Coding','eating','football'],
+//     greet:function(){
+//         alert("Hi there");
+//     }
+// }
+
+// delete person.age;
+
+// person.isAdmin =true;
+// console.log(person['first name']);
+const addMovieBtn =document.getElementById('add-movie-btn');
+const searchBtn =document.getElementById('search-btn');
+
+const movies =[];
+
+const renderMovies =()=>{
+    const movieList = document.getElementById('movie-list');
+    if(movies.length ===0){
+        movieList.classList.remove('visible');
+        return;
+    }else{
+        movieList.classList.add('visible');
     }
+
+    movieList.innerHTML ='';
+
+    movies.forEach((movie)=>{
+        const movieEl =document.createElement('li');
+        let text = movie.info.title + ' - ';
+        for(const key in movie.info){
+            if(key !=='title'){
+                text = text + `${key}: ${movie.info[key]}`
+            }
+        }
+
+        movieEl.textContent = text;
+        movieList.appendChild(movieEl);
+    });
 }
 
-delete person.age;
+const addMovieHandler =()=>{
+    const title =document.getElementById('title').value;
+    const extraName =document.getElementById('extra-name').value;
+    const extraValue =document.getElementById('extra-value').value;
+    if(title.trim()==='' || extraName.trim()==='' ||extraValue.trim() ===''){
+        return;
+    }
 
-person.isAdmin =true;
-console.log(person['first name']);
+    const newMovie ={
+        info:{
+            title,
+            [extraName]:extraValue
+        },
+        id:Math.random()
+    }
+
+    movies.push(newMovie);
+    console.log(newMovie);
+    renderMovies();
+}
+
+addMovieBtn.addEventListener('click',addMovieHandler);
