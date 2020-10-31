33611d13bd19c5b2641b6ffeb83f8de0c113d5d6
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-31 16:06:28 +0200

    Learnt how to fetch post from server by using fetch api and also posting data to the server

--- http-code-setup/assets/scripts/app.js
+++ http-code-setup/assets/scripts/app.js
@@ -5,39 +5,50 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url,data){
-const promise = new Promise((resolve, reject)=>{
+// const promise = new Promise((resolve, reject)=>{
 
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
+//xhr.setRequestHeader('Content-Type', 'application/json');
 
-xhr.open(method, url);
+// xhr.open(method, url);
 
-xhr.responseType ='json';
+// xhr.responseType ='json';
 
-xhr.onload =function(){
-    if(xhr.status >= 200 && xhr.status < 300){
-        resolve(xhr.response);
-    }else{
-        reject(new Error('Something went wrong...'));
-    }
+// xhr.onload =function(){
+//     if(xhr.status >= 200 && xhr.status < 300){
+//         resolve(xhr.response);
+//     }else{
+//         reject(new Error('Something went wrong...'));
+//     }
     
-//const listOfPost = JSON.parse(xhr.response);
-}
+// //const listOfPost = JSON.parse(xhr.response);
+// }
 
-xhr.onerror =function(){
-   reject(new Error('Failed to load send request'));
-}
+// xhr.onerror =function(){
+//    reject(new Error('Failed to load send request'));
+// }
 
-xhr.send(JSON.stringify(data));
+// xhr.send(JSON.stringify(data));
 
+// });
+
+// return promise;
+return fetch(url,{
+    method:method,
+    body:JSON.stringify(data),
+    headers:{
+        'Content-Type':'application/json'
+    }
+}).then(response =>{
+    console.log(response);
+    return response.json();
 });
-
-return promise;
 
 }
 
 async function fetchPost(){
     try{
-        const responseData = await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/post');
+        const responseData = await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts');
    
         const listOfPost = responseData;
         for(const post of listOfPost){
@@ -51,9 +62,7 @@ async function fetchPost(){
     
     }catch(error){
         alert(error.message);
-        
     }
- 
 }
 
 async function createPost(title, content){
@@ -66,7 +75,7 @@ async function createPost(title, content){
 
     sendHttpRequest('POST','https://jsonplaceholder.typicode.com/posts', post);
 
-}
+};
 
 fetchButton.addEventListener('click', fetchPost);
 
@@ -82,5 +91,5 @@ postList.addEventListener('click', event=>{
  if(event.target.tagName ==='BUTTON'){
      const postId =event.target.closest('li').id;
      sendHttpRequest("DELETE", `https://jsonplaceholder.typicode.com/posts/${postId}`)
-; }
+     }
 });
