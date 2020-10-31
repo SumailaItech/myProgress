d1418a12b2990ccfa2a46d870a9b6d0c489ac9b1
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-31 08:47:58 +0200

    learnt more about numbers

--- adv-fn-starting-setup/app.js
+++ adv-fn-starting-setup/app.js
@@ -93,4 +93,9 @@ function getFriendNames(person){
     return collectionNames;
 }
 
+function randomBetween(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(randomBetween(1,10));
 console.log(getFriendNames(myself));
