726409548719d3ec89a70ee2549947842c24618c
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-31 14:02:14 +0200

    Learnt how to modify the code with promise, async and await

--- http-code-setup/assets/scripts/app.js
+++ http-code-setup/assets/scripts/app.js
@@ -1,25 +1,42 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('singlePost');
 
+function sendHttpRequest(method, url){
+const promise = new Promise((resolve, reject)=>{
+
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
+xhr.open(method, url);
 
 xhr.responseType ='json';
 
 xhr.onload =function(){
+    resolve(xhr.response);
 //const listOfPost = JSON.parse(xhr.response);
-    const listOfPost = xhr.response;
-    for(const post of listOfPost){
-        const postEl = document.importNode(postTemplate.content, true);
-        //console.log(postEl);
-        postEl.querySelector('h2').textContent = post.title;
-        postEl.querySelector('p').textContent = post.body;
-
-        listElement.append(postEl);
-    }
 }
 
 xhr.send();
 
+});
 
+return promise;
+
+}
+
+async function fetchPost(){
+    const responseData = await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts');
+    console.log(responseData);
+    const listOfPost = responseData;
+    console.log(listOfPost);
+    for(const post of listOfPost){
+        const postEl = document.importNode(postTemplate.content, true);
+        //console.log(postEl);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+
+        listElement.append(postEl);
+    }
+
+}
+
+fetchPost();
