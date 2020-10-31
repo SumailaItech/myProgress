3307fff305b788e895f01f1ead44e068f0ec1d39
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-31 10:34:29 +0200

    Learnt more on the chaining of multiple promise and error handling techniques

--- async-starting-setup/app.js
+++ async-starting-setup/app.js
@@ -1,8 +1,72 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) =>{
+  const promise =new Promise((resolve,reject)=>{
+    navigator.geolocation.getCurrentPosition(success =>{
+      resolve(success);
+    }, error =>{
+      reject(error);
+    }, opts);
+    
+});
+return promise;
+};
+
+
+const setTimer =(duration)=>{
+  const promise = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    resolve('Done!');
+  }, duration);
+});
+return promise;
+}
+
 function trackUserHandler() {
-  console.log('Clicked!');
+  //console.log('Clicked!');
+// navigator.geolocation.getCurrentPosition(
+//     posData=>{
+//       // setTimeout(()=>{
+//       //   console.log(posData.coords);
+//       // },2000);
+//       setTimer(2000).then(data =>{
+//         console.log(data);
+//       });
+//   },
+//   error =>{
+//     console.log(error);
+//   }
+//   );
+  // setTimeout(()=>{
+  //   console.log('Timer done')
+  // }, 0);
+  let positionData;
+  getPosition().then(posData =>{
+    positionData = posData;
+    return setTimer(2000);
+  })
+  .catch(err =>{
+    console.log(err);
+  })
+  .then(data =>{
+    console.log(data, positionData);
+  });
+
+
+  setTimer(1000).then(data=>{
+   console.log(data);
+  });
+
+
+  console.log('Getting logging');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+let result = 0;
+for(let i =0; i<1000000; i++){
+  result += i;
+}
+
+console.log(result);
