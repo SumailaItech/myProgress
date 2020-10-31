99586ce6cd34910b78f6eca3624b262ce0f4e153
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-31 14:48:33 +0200

    Learnt how to delete a post from the server

--- http-code-setup/assets/scripts/app.js
+++ http-code-setup/assets/scripts/app.js
@@ -1,7 +1,8 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('singlePost');
 const form = document.querySelector('#new-post form');
-const fetchButton = document.querySelector('#available-posts button')
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url,data){
 const promise = new Promise((resolve, reject)=>{
@@ -27,15 +28,14 @@ return promise;
 
 async function fetchPost(){
     const responseData = await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts');
-    console.log(responseData);
+   
     const listOfPost = responseData;
-    console.log(listOfPost);
     for(const post of listOfPost){
         const postEl = document.importNode(postTemplate.content, true);
         //console.log(postEl);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body;
-
+        postEl.querySelector('li').id = post.id;
         listElement.append(postEl);
     }
 
@@ -61,4 +61,11 @@ form.addEventListener('submit', event =>{
     const enteredContent = event.currentTarget.querySelector('#content').value;
 
     createPost(enteredTitle, enteredContent);
-})
+});
+
+postList.addEventListener('click', event=>{
+ if(event.target.tagName ==='BUTTON'){
+     const postId =event.target.closest('li').id;
+     sendHttpRequest("DELETE", `https://jsonplaceholder.typicode.com/posts/${postId}`)
+; }
+});
