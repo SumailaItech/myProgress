79c741df31c9f9002531db1d1853b573a8935b9b
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-11-02 17:27:43 +0200

    I have convert the nodejs app to REST api by adding the REST Route to it

--- nodejs-practice/app.js
+++ nodejs-practice/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const locationRoutes = require('./routes/location');
+
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views','views');
+// app.set('view engine','ejs');
+// app.set('views','views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
-    res.setHeader('Content-Type','text/html');
-    next();
-});
+app.use(locationRoutes);
 
-app.use((req, res, next)=>{
-    const userName = req.body.username || 'Unknown user';
-    // res.send(`<h1>Hi ${userName}</h1>`)
-    res.render('index',{
-        user:userName
-    })
-});
+// app.use((req, res, next)=>{
+//     res.setHeader('Content-Type','text/html');
+//     next();
+// });
+
+// app.use((req, res, next)=>{
+//     const userName = req.body.username || 'Unknown user';
+//     // res.send(`<h1>Hi ${userName}</h1>`)
+//     res.render('index',{
+//         user:userName
+//     })
+// });
 
 app.listen(3000);
