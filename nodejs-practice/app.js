6762b43afb1b4e7a1e148e1c0774782869f99cdf
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-11-02 04:21:33 +0200

    Leartn how to render html page to the brower

--- nodejs-practice/app.js
+++ nodejs-practice/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next)=>{
     res.setHeader('Content-Type','text/html');
@@ -8,7 +14,11 @@ app.use((req, res, next)=>{
 });
 
 app.use((req, res, next)=>{
-    res.send('<h1>Hello World</h1>')
+    const userName = req.body.username || 'Unknown user';
+    // res.send(`<h1>Hi ${userName}</h1>`)
+    res.render('index',{
+        user:userName
+    })
 });
 
 app.listen(3000);
